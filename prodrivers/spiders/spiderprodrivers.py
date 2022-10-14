237dd5baac694c9362bda9f03d95879656cfe3f8
ricaportela <ricaportela@gmail.com>
2022-10-14 11:48:19 -0300

    not working jobRequirements and joBenefits

--- prodrivers/spiders/spiderprodrivers.py
+++ prodrivers/spiders/spiderprodrivers.py
@@ -6,8 +6,8 @@ from prodrivers.items import ProdriversItem
 
 class SpiderprodriversSpider(scrapy.Spider):
     name = "spiderprodrivers"
-    city = "California"
-    start_urls = ["https://www.prodrivers.com/jobs/?City=&State=California"]
+    city = "Washington"
+    start_urls = ["https://www.prodrivers.com/jobs/?City=&State=Washington"]
     custom_settings = {
         "FEED_EXPORT_FIELDS": [
             "title",
@@ -21,19 +21,19 @@ class SpiderprodriversSpider(scrapy.Spider):
     }
 
     def start_requests(self):
-        city = "California"
-        url = "https://www.prodrivers.com/jobs/?City=&State=California"
+        city = "Washington"
+        url = "https://www.prodrivers.com/jobs/?City=&State=Washington"
         yield scrapy.Request(url)
 
     def parse(self, response):
         item = ProdriversItem()
-        for selector in response.css(".accordionHead"):
-            item["title"] = selector.css("h2 ::text").get()
-            item["city"] = str(selector.css("h3 ::text").get()).strip()
-            item["basepay"] = selector.css(".basePay ::text").get()
-            item["shortDescription"] = response.xpath("/html/body/section[2]/div[2]/div/div[1]/div[1]/p[2]/text()")
-            item["jobDescription"] = response.xpath("/html/body/section[2]/div[2]/div/div[1]/div[2]/div/ul[1]/li/text()").extract()
-            item["jobRequirements"] = response.xpath("/html/body/section[2]/div[2]/div/div[1]/div[2]/div/ul[2]/li/text()").extract()
-            item["jobBenefits"] = response.xpath("/html/body/section[2]/div[2]/div/div[1]/div[2]/div/ul[3]/li/text()").extract()
+        for selector in response.css(".accordionItem"):
+            item["title"] = selector.css(".accordionHead h2 ::text").get()
+            item["city"] = str(selector.css(".accordionHead h3 ::text").get()).strip()
+            item["basepay"] = selector.css(".accordionHead .basePay ::text").get()
+            item["shortDescription"] = selector.css(".accordionHead .shortDescription ::text").get()
+            item["jobDescription"] =  [li.xpath('string(.)').extract()[0] for li in response.xpath('/html/body/section[2]/div[2]/div/div[1]/div[2]/div/ul[1]/li')]
+            item["jobRequirements"] =  [li.xpath('string(.)').extract()[0] for li in response.xpath('/html/body/section[2]/div[2]/div/div[1]/div[2]/div/ul[2]/li')]
+            item["jobBenefits"] =  [li.xpath('string(.)').extract()[0] for li in response.xpath('/html/body/section[2]/div[2]/div/div[1]/div[2]/div/ul[3]/li')]
 
-            yield item
+        yield item
